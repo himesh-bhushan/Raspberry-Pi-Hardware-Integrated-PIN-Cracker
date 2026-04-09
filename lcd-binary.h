4b5437fdfecbd5dc2b6fb47f7e1c8689cf5ba759
Himesh <101592195+himesh-bhushan@users.noreply.github.com>
2026-04-10 04:53:05 +0800

    build: hardware interface functions

--- lcd-binary.h
+++ lcd-binary.h
@@ -2,7 +2,6 @@
 #define LCD_BINARY_H
 
 #include <stdint.h>
-
 /* Core hardware/binary interface */
 void pin_mode(volatile uint32_t *gpio, int pin, int mode);
 void digital_write(volatile uint32_t *gpio, int pin, int value);
