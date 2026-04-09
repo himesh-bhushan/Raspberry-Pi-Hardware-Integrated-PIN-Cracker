97c540916f55b2ae6d3ed03d790c5a8d671a2fcb
Himesh <101592195+himesh-bhushan@users.noreply.github.com>
2026-04-10 04:47:33 +0800

    build: configuration for algorithm

--- config.h
+++ config.h
@@ -1,19 +1,9 @@
-#ifndef CW2_CONFIG_H
-#define CW2_CONFIG_H
-
-/* 
-   Config settings for the main app
-*/
-
-// HINT: you can use these flags to indicate which tasks are covered
+#ifndef CONFIG_H
+#define CONFIG_H
 #define VERSION 5
 #define VERSION_STR "arbitrary number of dimensions"
-
-// Enable interval-times
 #define ITIMER
-// Enable output to LCD display
 #define OUTPUT_LCD
-
 
 // simulated delay for a submission in micro-seconds
 #define SUBMIT_DELAY 20
@@ -23,17 +13,12 @@
 // constant: length of the sequence
 #define SEQL 3
 
-/* 
-   Wiring (see call to lcd-init() in main, using BCM numbering)
-*/
-
 // the green data LED
 #define LED 26
 // the red control LED
 #define LED2 5
 // the button
 #define BUTTON 19
-
 
 // GPIO pins used by the LCD
 #define LCD_PIN_RS 25
@@ -43,11 +28,7 @@
 #define LCD_PIN_D6 27
 #define LCD_PIN_D7 22
 
-/*
-  Delays and timings
-*/
-
-// all in micro-seconds
+//Delays and timings in micro-seconds
 #define DELAY   700000
 #define TIMEOUT 3000000
 
